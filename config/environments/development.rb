a3e7944442f400fc53f26917d254db19d6d35afb
Izoret <izoret@proton.me>
2025-11-26 12:23:07 +0100

    setup for https

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "antlia.dopolytech.fr:3000"
+  config.hosts << "polybanque.antlia.dopolytech.fr"
+
+  config.force_ssl = true
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
