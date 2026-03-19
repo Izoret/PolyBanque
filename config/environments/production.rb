00a9932d19da5b2f4b341936bac473f0dac0a0f0
raphael.izoret <raphael.izoret@etu.umontpellier.fr>
2026-03-19 11:47:02 +0100

    add hosts protection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "polybanque.izoret.fr"
+  config.hosts << "www.polybanque.izoret.fr"
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
