8c339714ef8c07d1fff09d849d3f2675f5a80710
Izoret <izoret@proton.me>
2026-08-08 12:19:30 +0200

    fix seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-puts "🌱 Seeding data..."
+puts "Destroying data"
 
-# 1. Clean the slate (Order matters due to foreign keys!)
 Participation.destroy_all
 Operation.destroy_all
 Membership.destroy_all
 Group.destroy_all
 User.destroy_all
+
+puts "🌱 Seeding data..."
 
 puts "Creating Users..."
 alice = User.create!(username: "Alice", email_address: "alice@example.com", password: "password123", password_confirmation: "password123")
@@ -27,46 +28,40 @@ Membership.create!(user: bob, group: ski_trip)
 Membership.create!(user: raph, group: ski_trip)
 Membership.create!(user: raph, group: italie)
 
-puts "Creating Operation: Welcome Dinner..."
-dinner_op = Operation.new!(
+puts "Creating Operations..."
+
+dinner_op = Operation.new(
   name: "Welcome Dinner",
   total_amount: 150.00,
   date: DateTime.now - 2.days,
   group: ski_trip,
   author: alice
 )
-
 [ alice, bob, raph ].each do |user|
-  Participation.create!(
-    user: user,
-    operation: dinner_op,
-    amount_share: 50.00
-  )
+  dinner_op.participations.build(user: user, amount_share: 50.00)
 end
-
 dinner_op.save!
 
-puts "Creating Operation: Gas..."
-gas_op = Operation.create!(
+gas_trip = Operation.new(
   name: "Gas for the drive up",
   total_amount: 60.00,
   date: DateTime.now - 3.days,
   group: ski_trip,
   author: bob
 )
+gas_trip.participations.build(user: bob, amount_share: 30.00)
+gas_trip.participations.build(user: raph, amount_share: 30.00)
+gas_trip.save!
 
-puts "Creating Operation: test..."
-gas_op = Operation.create!(
+gas_test = Operation.new(
   name: "test",
   total_amount: 60.00,
-  date: DateTime.now - 1.days,
+  date: DateTime.now - 1.day,
   group: ski_trip,
   author: bob
 )
-
-
-# Only Bob and Charlie participate in this cost
-Participation.create!(user: bob, operation: gas_op, amount_share: 30.00)
-Participation.create!(user: raph, operation: gas_op, amount_share: 30.00)
+gas_test.participations.build(user: bob, amount_share: 30.00)
+gas_test.participations.build(user: raph, amount_share: 30.00)
+gas_test.save!
 
 puts "✅ Done! Seeded #{User.count} users, #{Group.count} groups, and #{Operation.count} operations."
